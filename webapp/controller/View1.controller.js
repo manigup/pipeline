1d0ecfbafba69c25bdb7dabb4a2066c2eb44315d
Manish Gupta <gmanish478@gmail.com>
2022-01-12 10:32:02 +0000

    Second Commit

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -10,33 +10,49 @@ sap.ui.define([
 
         return Controller.extend("project1.controller.View1", {
             onInit: function () {
-                var oModel = new JSONModel();
-                var mock = {
+                var userDetails = {
                     firstname: "Dummy",
                     lastname: "User",
                     email: "dummy.user@com",
                     name: "dummy.user@com",
                     displayName: "Dummy User (dummy.user@com)"
                 };
-                oModel.loadData(this.getBaseURL() + "/user-api/currentUser");
-                oModel.dataLoaded()
-                    .then(() => {
-                        if (!oModel.getData().email) {
-                            oModel.setData(mock);
-                        }
-                        this.getView().setModel(oModel, "userInfo");
-                    })
-                    .catch(() => {
-                        oModel.setData(mock);
-                        this.getView().setModel(oModel, "userInfo");
-                    });
+                if (sap.ushell) {
+                    var userInfo = sap.ushell.Container.getService("UserInfo");
+                    userDetails.firstname = userInfo.getFirstName();
+                    userDetails.lastname = userInfo.getLastName();
+                    userDetails.email = userInfo.getEmail();
+                    userDetails.name = userDetails.firstname + " " + userDetails.lastname;
+                    userDetails.displayName = userInfo.getId();
+                }
+                this.getView().setModel(new JSONModel(userDetails), "userInfo");
+                // var oModel = new JSONModel();
+                // var mock = {
+                //     firstname: "Dummy",
+                //     lastname: "User",
+                //     email: "dummy.user@com",
+                //     name: "dummy.user@com",
+                //     displayName: "Dummy User (dummy.user@com)"
+                // };
+                // oModel.loadData(this.getBaseURL() + "/user-api/currentUser");
+                // oModel.dataLoaded()
+                //     .then(() => {
+                //         if (!oModel.getData().email) {
+                //             oModel.setData(mock);
+                //         }
+                //         this.getView().setModel(oModel, "userInfo");
+                //     })
+                //     .catch(() => {
+                //         oModel.setData(mock);
+                //         this.getView().setModel(oModel, "userInfo");
+                //     });
             },
 
-            getBaseURL: function () {
-                var appId = this.getOwnerComponent().getManifestEntry("/sap.app/id");
-                var appPath = appId.replaceAll(".", "/");
-                var appModulePath = jQuery.sap.getModulePath(appPath);
-                return appModulePath;
-            }
+            // getBaseURL: function () {
+            //     var appId = this.getOwnerComponent().getManifestEntry("/sap.app/id");
+            //     var appPath = appId.replaceAll(".", "/");
+            //     var appModulePath = jQuery.sap.getModulePath(appPath);
+            //     return appModulePath;
+            // }
         });
     });
